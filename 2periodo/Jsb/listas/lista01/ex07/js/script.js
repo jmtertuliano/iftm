3301bb298e3cf53196879f0d5d34c83c378a8629
Jonata de Matos Tertuliano <jmtertuliano@gmail.com>
2026-08-13 22:35:45 -0300

    Add script to display colored text based on RGB input

---
+++ 2periodo/Jsb/listas/lista01/ex07/js/script.js
@@ -0,0 +1,5 @@
+r = parseInt(prompt("Digite o valor de vermelho (R), entre 0 e 255:"))
+g = parseInt(prompt("Digite o valor de verde (G), entre 0 e 255:"))
+b = parseInt(prompt("Digite o valor de azul (B), entre 0 e 255:"))
+
+document.write(`<p style="color: rgb(${r}, ${g}, ${b});">Fundamentos de Web Design II</p>`)
